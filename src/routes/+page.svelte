a6f60c517034440e8586584138f23c4ac459cdcf
AbbasKagdi <abkagdibas@gmail.com>
2023-01-03 12:36:47 +0300

    stable tailwind + pokeA PI + Nav

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
+<script>
+    import {pokemon} from "../stores/pokestore"
+    console.log($pokemon)
+</script>
+
+<svelte:head>
+    <title>Pokedex</title>
+</svelte:head>
+
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+{#each $pokemon as poke}
+    <p>{poke.name}</p>
+{/each}
---
+++ src/stores/pokestore.js
@@ -0,0 +1,22 @@
+import {writable} from "svelte/store"
+
+// global context
+export const pokemon = writable([])
+
+const fetchPokemon = async () => {
+    const url = 'https://pokeapi.co/api/v2/pokemon?limit=150'
+    const res = await fetch(url)
+    const data = await res.json()
+
+    // populating index and image to pokemon data
+    const loadedPokemon = data.results.map((data, index) => {
+        return {
+            name: data.name,
+            id: index + 1,
+            image: 'https://raw.githubusercontent.com/pokeapi/sprites/master/sprites/pokemon/${index + 1}.png'
+        }
+    })
+    pokemon.set(loadedPokemon)
+}
+
+fetchPokemon()
