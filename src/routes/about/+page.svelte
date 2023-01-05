e2eaf1e35395782fc30da952f48798d1aaef3dbc
AbbasKagdi <abkagdibas@gmail.com>
2023-01-05 12:40:07 +0300

    filling about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,1 +1,2 @@
 <h1 class="text-4xl text-center my-8 uppercase">About</h1>
+<p class="text-center text-1xl">Welcome to the Pokedex. Here you will find all the information you need about Pokemons.</p>
